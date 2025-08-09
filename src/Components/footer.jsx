60e427a50ad679e13d3e7be1129df14af4c2952e
Azubuike Chima <chimafrancis94@gmail.com>
2025-08-10 00:00:51 +0100

    Worked on the footer section

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,10 +1,21 @@
-
-import React from 'react'
+import { FaGithub } from "react-icons/fa";
 
 function FooterComponent() {
   return (
-    <div>FooterComponent</div>
-  )
+    <footer className=" text-gray-300 pt-8 px-4 md:px-8">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="text-center md:text-left mb-4 md:mb-0">
+            <small className="text-white">Built by Azubuike Chima Francis</small> |
+            <a href="https://www.github.com/socheema">
+              <FaGithub className="inline ml-2 cursor-pointer text-gray-400 hover:text-white transition-colors" />
+            </a>{" "}|{" "}
+            <a href="https://www.github.com/socheema" className="text-sm cursor-pointer text-gray-300 hover:text-white transition-colors">Portfolio</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default FooterComponent
+export default FooterComponent;
